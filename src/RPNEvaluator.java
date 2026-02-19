cf9fbdd146901faa7c7c4ad68b366c2fae70f61a
Sina <sinayu2004@gmail.com>
2026-02-20 06:41:40 +0800

    Implemented evaluate method for RPN

--- src/RPNEvaluator.java
+++ src/RPNEvaluator.java
@@ -2,8 +2,47 @@ public class RPNEvaluator {
 
     public static void main(String[] args) {
 
-        System.out.println("Program started");
+        String expr = "3 4 + 2 *";
 
+        double result = evaluate(expr);
+
+        System.out.println("Result: " + result);
     }
 
+    public static double evaluate(String expression) {
+
+        Stack<Double> stack = new LinkedStack<>();
+
+        String[] tokens = expression.split(" ");
+
+        for (String token : tokens) {
+
+            if (token.equals("+") || token.equals("-") ||
+                    token.equals("*") || token.equals("/")) {
+
+                double b = stack.pop();
+                double a = stack.pop();
+
+                switch (token) {
+                    case "+":
+                        stack.push(a + b);
+                        break;
+                    case "-":
+                        stack.push(a - b);
+                        break;
+                    case "*":
+                        stack.push(a * b);
+                        break;
+                    case "/":
+                        stack.push(a / b);
+                        break;
+                }
+
+            } else {
+                stack.push(Double.parseDouble(token));
+            }
+        }
+
+        return stack.pop();
+    }
 }
